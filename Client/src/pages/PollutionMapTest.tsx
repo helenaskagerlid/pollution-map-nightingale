5d5b0c0199b990cecd0a5541dd7cbb6771a5ce79
helenaskagerlid <helenaskagerlid@gmail.com>
2024-10-28 18:47:19 +0100

    :fire: removed old code

--- Client/src/pages/PollutionMapTest.tsx
+++ Client/src/pages/PollutionMapTest.tsx
@@ -65,27 +65,6 @@ export const PollutionMapTest = () => {
               </Popup>
             </CircleMarker>
           ))}
-
-          {/* {selectedLocation && <MapUpdater center={selectedLocation.center} />}
-
-          {selectedLocation && (
-            <CircleMarker
-              center={selectedLocation.center}
-              radius={1}
-              fillColor="transparent"
-              color={getMarkerColor(selectedLocation.data.value)}
-              weight={8}
-              stroke={true}
-            >
-              <Popup autoPan={true}>
-                <strong>{selectedLocation.country}</strong>
-                <br />
-                <strong>PM₂.₅:</strong> {selectedLocation.data.value.toFixed(2)}{" "}
-                <strong>Date: </strong>
-                {selectedLocation.data.date}
-              </Popup>
-            </CircleMarker>
-          )} */}
         </MapContainer>
 
         <img
@@ -93,24 +72,6 @@ export const PollutionMapTest = () => {
           src="../../src/assets/grade.png"
           alt="Scale of PM2.5"
         />
-        <h3>About PM₂.₅ and Heart disease</h3>
-        <p>
-          PM₂.₅, or fine particulate matter, is linked to heart diseases due to
-          its ability to penetrate deep into the lungs and enter the
-          bloodstream. Exposure to high levels of PM₂.₅ can cause inflammation
-          in blood vessels, leading to conditions like atherosclerosis, which
-          increases the risk of heart attacks and strokes. Studies show that
-          both short-term and long-term exposure to PM₂.₅ can increase
-          cardiovascular mortality and the likelihood of acute heart issues in
-          at-risk individuals. This correlation highlights the importance of
-          reducing air pollution to prevent heart-related health problems.
-        </p>
-        <div className="image-container">
-          <img
-            src="../../src/assets/pollutionguide.png"
-            alt="Pollution guide"
-          />
-        </div>
       </div>
     </>
   );
