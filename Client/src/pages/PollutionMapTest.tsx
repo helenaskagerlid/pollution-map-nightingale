f0b745cdae3d9d0f71bfc52173eb534545a3ab05
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-10-29 12:49:46 +0100

    add right pollution map test page
    Co-authored-by: Helena Skagerlid <helenaskagerlid@users.noreply.github.com>

--- Client/src/pages/PollutionMapTest.tsx
+++ Client/src/pages/PollutionMapTest.tsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { IPlaces } from "../models/IPlaces";
-import { CircleMarker, Popup, TileLayer, useMapEvent } from "react-leaflet";
+import { CircleMarker, Popup, TileLayer } from "react-leaflet";
 import { MapContainer } from "react-leaflet";
 
 export const PollutionMapTest = () => {
   const [places, setPlaces] = useState<IPlaces[]>([]);
-  const [zoomLevel, setZoomLevel] = useState(4);
 
   const getMarkerColor = (value: number) => {
     if (value < 12.5) return "#8EFF44";
@@ -30,12 +29,22 @@ export const PollutionMapTest = () => {
   //   fetchData();
   // }, []);
 
-  const ZoomHandler = () => {
-    useMapEvent("zoomend", (event) => {
-      setZoomLevel(event.target.getZoom());
-    });
-    return null;
-  };
+  useEffect(() => {
+    const fetchData = async (): Promise<IPlaces[]> => {
+      try {
+        const response = await axios.get("http://localhost:3000/nightingale2");
+        const filteredData = response.data.filter(
+          (_: IPlaces, index: number) => index % 7 === 0
+        );
+        setPlaces(filteredData);
+        return filteredData;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+      }
+    };
+    fetchData();
+  }, []);
 
   console.log("Hämtad data", places);
   return (
@@ -50,39 +59,31 @@ export const PollutionMapTest = () => {
             scrollWheelZoom={true}
             // ref={mapRef}
           >
-            <ZoomHandler />
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            {places
-              .filter((place) => {
-                // Begränsa antalet renderade markörer beroende på zoomnivå
-                if (zoomLevel >= 8) return true; // Visa alla markörer från zoom 8 och uppåt
-                if (zoomLevel >= 5 && place.value >= 50) return true; // Vid zoom 5-7, visa markörer med högre PM2.5-värden
-                if (zoomLevel >= 3 && place.value >= 100) return true; // Vid zoom 3-4, visa endast de högsta värdena
-                return false; // Dölj markörer vid lägre zoomnivåer
-              })
-              .map((place, index) => (
-                <CircleMarker
-                  key={index}
-                  center={[place.latitude, place.longitude]}
-                  radius={1}
-                  fillColor="transparent"
-                  color={getMarkerColor(place.value)}
-                  weight={5}
-                  stroke={true}
-                >
-                  <Popup>
-                    <strong>{place.country}</strong>
-                    <br />
-                    <strong>PM₂.₅:</strong> {place.value.toFixed(2)}{" "}
-                    <strong>Date: </strong>
-                    {place.date}
-                  </Popup>
-                </CircleMarker>
-              ))}
+           
+          {places.map((place, index) => (
+            <CircleMarker
+              key={index}
+              center={[place.latitude, place.longitude]}
+              radius={1}
+              fillColor="transparent"
+              color={getMarkerColor(place.value)}
+              weight={5}
+              stroke={true}
+            >
+              <Popup>
+                <strong>{place.country}</strong>
+                <br />
+                <strong>PM₂.₅:</strong> {place.value.toFixed(2)}{" "}
+                <strong>Date: </strong>
+                {place.date}
+              </Popup>
+            </CircleMarker>
+          ))}
           </MapContainer>
 
           <img
